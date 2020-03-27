6a142efe207252c9fd884ac4ca23af59956b49d7
Guillem Rochina <guillem.rochinaaguas@gmail.com>
2020-03-27 20:38:51 +0100

    Exploración

--- ExploracionGuillem.py
+++ ExploracionGuillem.py
@@ -12,6 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+
 archives = !ls | grep 'df'
 
 df = pd.read_csv(archives[0])
@@ -19,11 +21,13 @@ df = pd.read_csv(archives[0])
 for i in range(1, len(archives)):
     df = df.append(pd.read_csv(archives[i]))
    
-df.drop(['number_of_reviews_ltm', 'license', 'Unnamed: 0'], axis = 1, inplace = True)
+df.drop(['number_of_reviews_ltm', 'license', 'Unnamed: 0', 'requires_license'], axis = 1, inplace = True)
     
 df.groupby(['date', 'id'])['goodprice'].count()[df.groupby(['date', 'id'])['goodprice'].count()>1]
 
 df.drop_duplicates(subset = ['date', 'id'], inplace = True)
+
+df.drop('price', axis = 1, inplace = True)
 
 df.isnull().sum()[df.isnull().sum()>0]
 
@@ -76,6 +80,10 @@ def who(x):
 who(10425744)
 
 fig, ax = plt.subplots(1, 1, figsize = (20, 10))
+sns.distplot(df['PricePNight'], bins = 13)
+plt.tight_layout()
+
+fig, ax = plt.subplots(1, 1, figsize = (20, 10))
 sns.distplot(np.log(df['PricePNight']), bins = 13)
 plt.tight_layout()
 
@@ -83,3 +91,103 @@ fig, ax = plt.subplots(1, 1, figsize = (20, 10))
 plt.plot(df.groupby('date')['PricePNight'].mean().index, df.groupby('date')['PricePNight'].mean(), color = "red")
 plt.xticks(df.groupby('date')['PricePNight'].mean().index, rotation = 75)
 plt.tight_layout()
+
+df['LogPricePNight'] = np.log(df['PricePNight'])
+
+# Pareix que a partir de 12 llits son outliers respecte al preu amb prou Leverage
+
+fig, ax = plt.subplots(1, 1, figsize = (15, 10))
+plt.scatter(df['bedrooms'], df['LogPricePNight'], color = "navy", alpha = 0.05)
+sns.regplot('bedrooms', 'LogPricePNight', data = df, scatter = False, color = "red")
+plt.tight_layout()
+
+# El mateix pasa a partir de 8 banys
+
+fig, ax = plt.subplots(1, 1, figsize = (15, 10))
+plt.scatter(df['bathrooms'], df['LogPricePNight'], color = "navy", alpha = 0.05)
+sns.regplot('bathrooms', 'LogPricePNight', data = df, scatter = False, color = "red")
+plt.tight_layout()
+
+# Crec que hi ha colinealitat entre bathrooms y bedrooms
+fig, ax = plt.subplots(1, 1, figsize = (15, 10))
+plt.scatter(df['bathrooms'], df['bedrooms'], color = "navy", alpha = 0.05)
+sns.regplot('bathrooms', 'bedrooms', data = df, scatter = False, color = "red")
+plt.tight_layout()
+
+np.corrcoef(df['bedrooms'], df['bathrooms'])
+
+corrcolumns = list(df.dtypes[df.dtypes != 'object'].index)
+
+matrixcorrA = df[corrcolumns[1:21]].corr()
+
+sns.heatmap(matrixcorrA, vmin = -1, vmax = 1, cmap = "viridis", linecolor = "black")
+
+matrixcorrB = df[corrcolumns[21:41]].corr()
+
+sns.heatmap(matrixcorrB, vmin = -1, vmax = 1, cmap = "viridis", linecolor = "black")
+
+matrixcorrC = df[corrcolumns[41:]].corr()
+
+sns.heatmap(matrixcorrC, vmin = -1, vmax = 1, cmap = "viridis", linecolor = "black")
+
+fig, ax = plt.subplots(1, 1, figsize = (30, 30))
+sns.heatmap(df[corrcolumns[1:]].corr(), vmin = -1, vmax = 1, center = 0, cmap = "RdBu", ax = ax, annot = True,  annot_kws = {"size": 8})
+plt.show()
+
+# EL DE BAIX ES PER FER UNA MATRIU TRIANGULAR INFERIOR PER ESTALVIAR LA PART SUPERIOR QUE ES INNECESARIA
+mask = np.zeros_like(df[corrcolumns[1:]].corr())
+mask[np.triu_indices_from(mask)] = True
+fig, ax = plt.subplots(1, 1, figsize = (35, 30))
+sns.heatmap(df[corrcolumns[1:]].corr(), vmin = -1, vmax = 1, center = 0, mask = mask, cmap = "RdBu", ax = ax, annot = True,  annot_kws = {"size": 8})
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+X = df[['accommodates', 'instant_bookable', 'review_scores_rating', 'host_total_listings_count', 'Smoking allowed', 'bathrooms', 'beds', 'cleaning_fee', 'guests_included', 'availability_365', 'Air conditioning', 'Family/kid friendly']].values
+
+y = df['LogPricePNight'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
+
+lineareg = LinearRegression()
+
+lineareg.fit(X_train, y_train)
+
+lineareg.score(X_test, y_test)
+
+# HO FAIG AMB TOTES LES VARIABLES
+X_total = df[corrcolumns[1:-3]].values
+
+X_train, X_test, y_train, y_test = train_test_split(X_total, y, test_size = 0.3, random_state = 1997)
+
+lineareg = LinearRegression()
+
+lineareg.fit(X_train, y_train)
+
+lineareg.score(X_test, y_test)
+
+# PROBEM AMB UNA RIDGE
+from sklearn.linear_model import Ridge
+
+ridreg = Ridge(random_state = 1997)
+ridreg.fit(X_train, y_train)
+
+ridreg.score(X_test, y_test)
+
+for n,v in zip(corrcolumns[1:-3], lassoreg.coef_):
+    print('{} -> {}'.format(n, v))
+    
+# UTILITZE NOMÉS ELS ATRIBUTS DE LA RIDGE
+
+X = df[['host_is_superhost', 'host_has_profile_pic', 'cleaning_fee', 'guests_included', 'minimum_nights', 'availability_365', 'number_of_reviews', 'review_scores_accuracy']]
+y = df['LogPricePNight'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
+
+lineareg = LinearRegression()
+
+lineareg.fit(X_train, y_train)
+
+lineareg.score(X_test, y_test)
+# No té massa sentit el que diu la RIDGE
