add438dcdbf9fb4d98cfb159b47d44387695475c
Guillem Rochina <guillem.rochinaaguas@gmail.com>
2020-04-01 20:53:01 +0200

    Millorant Regressió Lineal

--- ExploracionGuillem.py
+++ ExploracionGuillem.py
@@ -6,13 +6,12 @@ Created on Thu Mar 26 22:19:56 2020
 @author: guillem
 """
 
+cd ~/DadesAirBNB
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
 
 archives = !ls | grep 'df'
 
@@ -21,17 +20,16 @@ df = pd.read_csv(archives[0])
 for i in range(1, len(archives)):
     df = df.append(pd.read_csv(archives[i]))
    
-df.drop(['number_of_reviews_ltm', 'license', 'Unnamed: 0', 'requires_license', 'availability_365'], axis = 1, inplace = True)
+df.drop(['number_of_reviews_ltm', 'license', 'Unnamed: 0', 'requires_license', 'availability_365', 'price'], axis = 1, inplace = True)
 df['date'] = pd.to_datetime(df['date'])
 df['Year'] = df['date'].dt.year
 df['Month'] = df['date'].dt.month    
 df['Day'] = df['date'].dt.day 
 
-df.groupby(['id', 'date','goodprice'])['goodprice'].count()
+df.groupby(['id', 'date','goodprice'])['goodprice'].count()[df.groupby(['id', 'date','goodprice'])['goodprice'].count()>1]
 
 df.drop_duplicates(subset = ['date', 'id', 'goodprice'], inplace = True)
 
-df.drop('price', axis = 1, inplace = True)
 
 df.isnull().sum()[df.isnull().sum()>0]
 
@@ -57,12 +55,9 @@ plt.tight_layout()
 
 df[['goodprice']].describe()
 
-fig, ax = plt.subplots(1, 1, figsize = (20, 10))
-sns.boxplot(df['neighbourhood_group_cleansed'], df['goodprice'], whis = 10)
-plt.tight_layout()
-
 df[(df['goodprice']>300)&(df['minimum_nights']>1)][['id', 'goodprice', 'minimum_nights']]
 
+# Calculem el PricePNight
 df['PricePNight'] = [price/minnight if (price > 300)&(minnight > 1) else price for price, minnight in zip(df['goodprice'], df['minimum_nights'])]
 
 df[(df['goodprice']>300)&(df['minimum_nights']>1)&(df['PricePNight']<30)][['id', 'goodprice', 'minimum_nights', 'PricePNight']].sort_values(['PricePNight'])
@@ -72,16 +67,16 @@ df[(df['goodprice']>300)&(df['minimum_nights']>1)&(df['PricePNight']<30)&(df['mi
 
 # HE DESCOBERT QUE ELS QUE TENEN MÉS DE 28 DÍES DE MINIMUM_NIGHTS FIQUEN EL PREU MENSUAL
 
-df[(df['minimum_nights']>28)&(df['goodprice']>20)&(df['PricePNight']< 10)][['goodprice', 'PricePNight']] # Estes files sobren
+df[(df['minimum_nights']>28)&(df['goodprice']>20)&(df['PricePNight']< 20)][['goodprice', 'PricePNight']] # Estes files sobren
 
-df.drop(df[(df['minimum_nights']>28)&(df['goodprice']>20)&(df['PricePNight']< 10)].index, inplace = True)
+df.drop(df[(df['minimum_nights']>28)&(df['goodprice']>20)&(df['PricePNight']< 20)].index, inplace = True)
 
 #Buscador per ficar al google chrome o firefox
 def who(x):
     print('\n', df[df['id']==x]['name'].unique(), '\n')
     return 'https://www.airbnb.es/rooms/' + str(x)
 
-who(10425744)
+who(199651)
 
 fig, ax = plt.subplots(1, 1, figsize = (20, 10))
 sns.distplot(df['PricePNight'], bins = 13)
@@ -96,6 +91,7 @@ plt.plot(df.groupby('date')['PricePNight'].mean().index, df.groupby('date')['Pri
 plt.xticks(df.groupby('date')['PricePNight'].mean().index, rotation = 75)
 plt.tight_layout()
 
+# FINALMENT CREE EL LOGPRICEPERNIGHT
 df['LogPricePNight'] = np.log(df['PricePNight'])
 df.drop(['goodprice'], axis = 1, inplace = True)
 
@@ -103,6 +99,15 @@ fig, ax = plt.subplots(1, 1, figsize = (20, 10))
 plt.plot(df.groupby('date')['LogPricePNight'].mean().index, df.groupby('date')['LogPricePNight'].mean(), color = "red")
 plt.xticks(df.groupby('date')['LogPricePNight'].mean().index, rotation = 75)
 plt.tight_layout()
+
+# Construim les categories
+factorcolumns = [x for x in list(df.dtypes[df.dtypes == 'int'].index) if 0 in df[x].value_counts().index]
+df[factorcolumns] = df[factorcolumns].astype('category')
+
+# DEURÍEM AFEGIR ELS MESOS 1, 2 i 3?                             
+sns.pointplot('Month', 'PricePNight', hue = 'Year', data = df)
+
+
 # Pareix que a partir de 12 llits son outliers respecte al preu amb prou Leverage
 
 fig, ax = plt.subplots(1, 1, figsize = (15, 10))
@@ -110,12 +115,15 @@ plt.scatter(df['bedrooms'], df['LogPricePNight'], color = "navy", alpha = 0.05)
 sns.regplot('bedrooms', 'LogPricePNight', data = df, scatter = False, color = "red")
 plt.tight_layout()
 
+
+fig, ax = plt.subplots(1, 1, figsize = (15, 10))
+sns.pairplot(
 # El mateix pasa a partir de 8 banys
 
 fig, ax = plt.subplots(1, 1, figsize = (15, 10))
 plt.scatter(df['bathrooms'], df['LogPricePNight'], color = "navy", alpha = 0.05)
 sns.regplot('bathrooms', 'LogPricePNight', data = df, scatter = False, color = "red")
-plt.tight_layout()
+
 
 # Crec que hi ha colinealitat entre bathrooms y bedrooms
 fig, ax = plt.subplots(1, 1, figsize = (15, 10))
@@ -131,10 +139,13 @@ columnselection = ["host_response_time", "neighbourhood_group_cleansed", "proper
 X_raw = pd.get_dummies(df, columns = columnselection)
 
 
-factorcolumns = [x for x in list(X_raw.dtypes[(X_raw.dtypes == 'int')| (X_raw.dtypes == 'uint8')].index) if 0 in X_raw[x].value_counts().index]
+factorcolumns = [x for x in list(X_raw.dtypes[(X_raw.dtypes == 'uint8')].index) if 0 in X_raw[x].value_counts().index]
 
 X_raw[factorcolumns] = X_raw[factorcolumns].astype('category')
+X_raw.dtypes[X_raw.dtypes == 'category']
 
+# ELIMINEM LA COLUMNA ID QUE NO ES IMPORTANT
+X_raw.drop('id', inplace = True, axis = 1)
 
 corrcolumns = list(X_raw.dtypes[(X_raw.dtypes == 'float') | (X_raw.dtypes == 'int')].index)
 
@@ -195,7 +206,7 @@ plt.show()
 for column in X_raw.dtypes[X_raw.dtypes == "category"].index.values:
     print(column, 'LogPricePNight', scipy.stats.pointbiserialr(X_raw[column], X_raw['LogPricePNight'])[0])
 
-
+# REGRESIÓ LINEAL
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -210,19 +221,33 @@ lineareg = LinearRegression()
 lineareg.fit(X_train, y_train)
 
 lineareg.score(X_test, y_test)
-X_raw.dtypes.
-selection = list(X_raw.dtypes[(X_raw.dtypes != 'O') & (X_raw.columns != 'date')].index)
 
+
+# HO FAIG AMB TOTES LES VARIABLES
+X_copy = X_raw.copy()
+
+selection = list(X_raw.dtypes[(X_raw.dtypes != 'O') & (X_raw.columns != 'date')].index)
+selectnum = list(X_raw.dtypes[(X_raw.dtypes != 'O') & (X_raw.columns != 'date') & (X_raw.dtypes != 'category')].index)
+
+atrinum = pd.Series(selectnum)[pd.Series(selectnum).str.contains('rice') == False][1:-3].tolist()
+atrinum
 atricompl = pd.Series(selection)[pd.Series(selection).str.contains('rice') == False].tolist()
 
-X = X_raw[atricompl].values
-# HO FAIG AMB TOTES LES VARIABLES
-
+X = X_copy[atricompl]
+y = df['LogPricePNight']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
 
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+X_train[atrinum] = ss.fit_transform(X_train[atrinum])
+X_train = X_train.values
 lineareg = LinearRegression()
 
 lineareg.fit(X_train, y_train)
+
+X_test[atrinum] = ss.transform(X_test[atrinum])
+
+X_test = X_test.values
 
 lineareg.score(X_test, y_test)
 
@@ -239,27 +264,12 @@ for n,v in zip(atricompl, ridreg.coef_):
     if np.abs(v) > 0:
         ridgeatributos.append(n)
 
-X = df[ridgeatributos].values
-y = df['LogPricePNight'].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
-
-ridatrreg = LinearRegression()
-ridatrreg.fit(X_train, y_train)
-
-ridatrreg.score(X_test, y_test)
-
 serieatributos = pd.DataFrame(zip(ridgeatributos, ridreg.coef_, np.abs(ridreg.coef_)), columns = ['Atributos', 'Coeficiente', 'ValorAbsoluto']).set_index('Atributos').sort_values('ValorAbsoluto', ascending = False)
 serieatributos[:30]
 
-# VAIG A PROBRAR UNA ELASTIC NET
+# VAIG A PROBRAR UNA ELASTIC NET (UNA MIERDA)
 
 from sklearn.linear_model import ElasticNet
-
-X = df[atricompl].values
-y = df['LogPricePNight'].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
 
 LinEN = ElasticNet()
 LinEN.fit(X_train, y_train)
@@ -269,38 +279,33 @@ for atr, coef in zip(atricompl, LinEN.coef_):
     if np.abs(v) > 0:
         atributos.append(atr)
 
-serieatributos = pd.DataFrame(zip(atributos, LinEN.coef_, np.abs(LinEN.coef_)), columns = ['Atributos', 'Coeficiente', 'ValorAbsoluto']).set_index('Atributos').sort_values('ValorAbsoluto', ascending = False)
+serieatributos = pd.DataFrame(zip(atributos, LinEN.user = !whoamicoef_, np.abs(LinEN.coef_)), columns = ['Atributos', 'Coeficiente', 'ValorAbsoluto']).set_index('Atributos').sort_values('ValorAbsoluto', ascending = False)
 serieatributos[:30]
 
+# FINALMENT PROBE AMB UNA LASSO
 
-atributosfinal = list(serieatributos[:30].index)
+from sklearn.linear_model import LassoCV
 
-X = X_raw[atributosfinal].values
-y = X_raw['LogPricePNight'].values
+lcv = LassoCV()
 
+lcv.fit(X_train, y_train)
+
+lcv.score(X_test,y_test)
+
+lastatributes = X_copy[atricompl].columns[np.abs(lcv.coef_) > 0].tolist()
+
+X = X_copy[lastatributes]
+y = df['LogPricePNight']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1997)
 
-finallinreg = LinearRegression()
-finallinreg.fit(X_train, y_train)
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+X_train = ss.fit_transform(X_train)
+X_train = X_train
+lineareg = LinearRegression()
 
-finallinreg.score(X_test, y_test) # Esta es la millor regresión que hi ha per ara
+lineareg.fit(X_train, y_train)
+X_test = ss.fit_transform(X_test)
 
-y_pred = finallinreg.predict(X_test)
+lineareg.score(X_test, y_test)
 
-from sklearn.metrics import mean_squared_error
-
-np.sqrt(mean_squared_error(y_test, y_pred))
-
-np.sqrt(mean_squared_error(np.exp(y_test), np.exp(y_pred)))
-
-# VAIG A PROBAR AMB STATSMODELS
-
-import statsmodels.regression.linear_model as sm
-X = df[atricompl].values
-y = df['LogPricePNight']
-X2 = np.append(arr = np.ones((len(X), 1)).astype(int), values = X, axis = 1)
-
-X_train, X_test, y_train, y_test = train_test_split(X2, y, test_size = 0.3, random_state = 1997)
-reg_ols = sm.OLS(endog = y_train, exog = X_train).fit()
-
-reg_ols.summary()
